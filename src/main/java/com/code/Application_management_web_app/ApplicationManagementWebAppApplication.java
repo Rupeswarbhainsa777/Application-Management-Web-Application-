b3139ac4ebabf24cbbffdb678362e77dbcd1309f
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2025-07-09 21:44:31 +0530

    first commit

---
+++ src/main/java/com/code/Application_management_web_app/ApplicationManagementWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.code.Application_management_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationManagementWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationManagementWebAppApplication.class, args);
+	}
+
+}
