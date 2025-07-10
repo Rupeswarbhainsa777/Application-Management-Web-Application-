7cdeed728874329d43d805cbc2fae4b4d50914e3
Rupeswar Bhainsa <rupeswar77666@gmail.com>
2025-07-11 01:46:24 +0530

    third

--- src/main/java/com/code/Application_management_web_app/controller/JobController.java
+++ src/main/java/com/code/Application_management_web_app/controller/JobController.java
@@ -3,7 +3,10 @@ package com.code.Application_management_web_app.controller;
 import com.code.Application_management_web_app.Entity.JobManagement;
 import com.code.Application_management_web_app.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/job")
@@ -15,5 +18,16 @@ public class JobController {
     @PostMapping("/add")
     public JobManagement addJob(@RequestBody JobManagement job) {
         return jobService.saveJob(job);
+    }
+
+    @GetMapping("/allJobs")
+    public ResponseEntity<List<JobManagement>> allJob(){
+        return jobService.getAllJobs();
+
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable int id) {
+        return jobService.deleteJobById(id);
     }
 }
--- src/main/java/com/code/Application_management_web_app/service/JobService.java
+++ src/main/java/com/code/Application_management_web_app/service/JobService.java
@@ -4,7 +4,13 @@ import com.code.Application_management_web_app.Entity.JobManagement;
 import com.code.Application_management_web_app.repositiory.JobRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class JobService {
@@ -16,5 +22,26 @@ public class JobService {
 
     public JobManagement saveJob(JobManagement job) {
         return jobRepository.save(job);
+    }
+
+
+
+    public ResponseEntity<List<JobManagement>> getAllJobs() {
+        try {
+            return new ResponseEntity<>(jobRepository.findAll(), HttpStatus.OK);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<String> deleteJobById(int id) {
+        if (jobRepository.existsById(id)) {
+            jobRepository.deleteById(id);
+            return ResponseEntity.ok("Job with ID " + id + " deleted successfully.");
+        } else {
+            return ResponseEntity.status(404).body("Job with ID " + id + " not found.");
+        }
     }
 }
